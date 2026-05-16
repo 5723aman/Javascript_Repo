f1c46761df48363a7de2bda5ab2821a01afde67e
Aman Kumar Singh <152841289+5723aman@users.noreply.github.com>
2026-05-16 20:03:09 +0000

    commit the changes

--- 02_basics/01_functions.js
+++ 02_basics/01_functions.js
@@ -37,3 +37,65 @@ function logginUser(username){
     return `${username} just logged in`
 }
 console.log(logginUser("Aman")) // if you dont pass any argument here it will give undefined
+
+
+//when we have to add more than one number then we will use REST opertor
+
+// function addCart(...num1){
+//     return num1;
+
+// }
+// console.log(addCart(200,3000, 4000));
+
+
+
+
+
+function addCart(val1,val2,...num1){
+    return num1; // here the output will be 4000, 6000 because val1 is 200 and val2 is 3000
+
+}
+console.log(addCart(200,3000, 4000, 6000));
+
+
+
+
+
+
+
+// how to pass object in the functiom
+
+const user={
+    name:"Aman",
+    age:23
+}
+
+function details(anyData){
+    console.log(`my name is ${anyData.name} and my age is ${anyData.age}`);
+}
+
+// details(user)    // ← "user" object is passed as an argument
+//   //↓
+// function details(anyData) {  // ← "anyData" is the parameter that receives "user"
+//     console.log(`my name is ${anyData.name} and my age is ${anyData.age}`);
+//     // anyData is now the user object, so this logs:
+//     // "my name is Aman and my age is 23"
+//}
+
+
+
+
+
+// passing the array in the function
+
+const arr=[1,2,3,4,5];
+
+function getArray(anyData){
+    return anyData[0];
+}
+
+//console.log(getArray(arr)) // here we are passing the array as an argument and it will return the first element of the array which is 1
+console.log(getArray([10,20,30,40,50])) // here we are passing the array directly as an argument and it will return the first element of the array which is 10
+
+
+
