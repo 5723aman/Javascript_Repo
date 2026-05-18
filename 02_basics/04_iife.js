9eadcfa1f99817aa191f672a7f4af59b6b87db83
Aman Kumar Singh <152841289+5723aman@users.noreply.github.com>
2026-05-18 19:06:24 +0000

    commit it

---
+++ 02_basics/04_iife.js
@@ -0,0 +1,13 @@
+(function chai (){
+    console.log("My name is aman")
+}) (); // named iife
+
+
+// arrow function
+
+( (name) =>{
+    console.log(`My name is khushi and ${name}`)
+})("aman") // unnamed iife with parameters
+
+
+// when we write ti=wo iifes together then after endig one we will clos it by semicolon(;).
