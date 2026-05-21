d7f782cd3bc141ada888665ba9fe2add40eb17a0
Aman Kumar Singh <152841289+5723aman@users.noreply.github.com>
2026-05-21 21:06:08 +0000

    Today i learned about control flow and will continue tomorrow

---
+++ 03_contolflow/one.js
@@ -0,0 +1,58 @@
+// if condition = in this the condition should be true to run the code inside the scope
+
+// if(2==2){
+//     console.log("Aman");
+// }
+// console.log("it will always get executed")
+
+// if(2==="2"){
+//     console.log("Aman");
+// }
+// console.log("it will always get executed")
+
+// [<,>, >=,<=,==, ===, !=, !==]
+
+
+// const tem=41;
+
+// if(tem<50){
+//     console.log("temperatire is less")
+// }
+// console.log ("  i will always get executed regatdless of condition")
+
+
+// const temp=41;
+
+// if(temp>50){
+//     console.log("temperatire is less")
+// }
+// console.log ("  i will always get executed regatdless of condition")
+
+
+// const tem=41;
+
+// if(tem<50){
+//     console.log("temperatire is less")
+// }
+// else{
+//     console.log("weither if will get executed or else will get executed depend upon the condtion")
+// }
+// console.log ("  i will always get executed regatdless of condition")
+
+
+
+
+
+// const p=200;
+
+// if(p>100){
+//     let power="fly";
+//     console.log(`i can ${power}`)
+// }
+// console.log(`i can ${power}`) // this will give problem because power is defined inside the scopw which is not accisseble outside the scope
+
+
+
+//NESTING
+
+const balance=1000;
